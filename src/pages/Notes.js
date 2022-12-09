0138a893b3c36eeaf4d0acc58ffdedb5758c171c
feizal reza <feizalreza@MacBook-Pro-feizal.local>
2022-12-09 23:03:49 +0700

    update css

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,7 @@ import SideBar from "../components/sideBar";
 import Navbar from "../components/Notes/Navbar";
 import AddNote from "../components/Notes/AddNotes";
 import NotesList from "../components/Notes/NotesList";
+import "../style/notes.css"
 
 const Notes = () => {
     return (
